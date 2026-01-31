8667a883956aa941757468cbb293f6f01e7684fc
Zere Yesenkhozha <zere@MacBook-Air-Zere-3.local>
2026-01-31 15:57:25 +0500

    9th commit

--- Python_basics/numbers.py
+++ Python_basics/numbers.py
@@ -5,3 +5,12 @@ z = 1j #complex
 print(type(x))
 print(type(y))
 print(type(z))
+
+#convert from int to float:
+a = float(x)
+
+#convert from float to int:
+b = int(y)
+
+#convert from int to complex:
+c = complex(x)
